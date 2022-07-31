921a84f9902ee4fe61189414f3c844ddedba6500
Mays AL-Azzawi <AB0168@student.jamk.fi>
2022-08-01 01:44:27 +0300

    my react projec

---
+++ run.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+
+#pull all thge files from git if someone else work with you
+git  pull
+
+# check the status of your  project
+
+git status
+
+#add thew new files 
+git add   src/*
+
+# commit your changes
+
+git commit -m "new changes"
+
+# push the changes to your branch and can latter merge the main branch
+
+git push 
